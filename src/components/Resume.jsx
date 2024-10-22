c4ff5018dacd30f2b34f1de27b6b2e57062e6e72
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-10-22 16:00:59 +0530

    working on displaying the pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const Resume = () => {
   return (
     <div style={{ width: '100%', height: '100vh' }}>
-      <iframe
-        src={`${process.env.PUBLIC_URL}/Himanshu_Resume.pdf`}
-        style={{ width: '100%', height: '100vh', border: 'none' }}
-        title="Resume"
+      <embed
+        // src={`${process.env.PUBLIC_URL}/Himanshu_Resume.pdf`}
+        src='Himanshu_Resume.pdf'
+        type="application/pdf"
+        width="100%"
+        height="100%"
       />
     </div>
   );
